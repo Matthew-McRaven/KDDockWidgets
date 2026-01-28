48742dc0d7d9258e9c51f546bf5272b99ba94c4d
Matthew McRaven <matthew.mcraven@gmail.com>
2026-01-27 21:08:26 -0800

    Stop using vendor'ed nlohmann/json
    Trust that the build system will otherwise provide the target.

--- src/nlohmann.cmake
+++ src/nlohmann.cmake
@@ -12,10 +12,5 @@ find_package(nlohmann_json QUIET)
 
 # Function to link to nlohmann
 function(link_to_nlohman target)
-    if(nlohmann_json_FOUND)
-        target_link_libraries(${target} PRIVATE nlohmann_json::nlohmann_json)
-    else()
-        message(STATUS "nlohmann_json not found in system. Using our own bundled one")
-        target_include_directories(${target} SYSTEM PRIVATE ${KKDockWidgets_PROJECT_ROOT}/src/3rdparty/nlohmann)
-    endif()
+    target_link_libraries(${target} PRIVATE nlohmann_json::nlohmann_json)
 endfunction()
